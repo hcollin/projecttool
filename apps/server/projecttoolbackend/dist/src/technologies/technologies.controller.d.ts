20f297e5235cc1dbc851089002ecb9abb693a683
Henrik Collin <hcollin78@gmail.com>
2025-11-23 16:03:05 +0200

    Initial nest.js server

---
+++ apps/server/projecttoolbackend/dist/src/technologies/technologies.controller.d.ts
@@ -0,0 +1,5 @@
+import { ITechnology } from "@frosttroll/projecttoolmodels";
+export declare class TechnologiesController {
+    getAllTechnologies(): ITechnology[];
+}
+//# sourceMappingURL=technologies.controller.d.ts.map
