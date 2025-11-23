b071859c2662dc0798f5ba6473bff2ddc9f58f8b
Henrik Collin <hcollin78@gmail.com>
2025-11-24 01:02:32 +0200

    Database is initialized for Technologies on startup.

--- apps/server/projecttoolbackend/src/technology/technology.module.ts
+++ apps/server/projecttoolbackend/src/technology/technology.module.ts
@@ -1,9 +1,13 @@
 import { Module } from "@nestjs/common";
 import { TechnologyController } from "./technology.controller";
 import { TechnologyService } from "./technology.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { TechnologyEntity } from "./technology.entity";
 
 @Module({
+    imports: [TypeOrmModule.forFeature([TechnologyEntity])],
     controllers: [TechnologyController],
     providers: [TechnologyService],
+    exports: [TypeOrmModule]
 })
 export class TechnologyModule {}
--- apps/server/projecttoolbackend/src/technology/technology.service.ts
+++ apps/server/projecttoolbackend/src/technology/technology.service.ts
@@ -1,22 +1,32 @@
 import { ITechnology } from "@frosttroll/projecttoolmodels/dist/src/index.js";
 import { Injectable } from "@nestjs/common";
 import { DEFAULT_TECHNOLOGIES } from "./default_technologies";
+import { InjectRepository } from "@nestjs/typeorm";
+import { TechnologyEntity } from "./technology.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class TechnologyService {
     private techs: Map<string, ITechnology> = new Map<string, ITechnology>();
+
+    private technologyRepository: Repository<TechnologyEntity>;
 
     /**
      * Constructor
      *
      * Initializes the technology service with default technologies.
      */
-    constructor() {
-        this.techs = new Map<string, ITechnology>();
-        const defaultTechs: ITechnology[] = DEFAULT_TECHNOLOGIES;
-        defaultTechs.forEach((tech) => {
-            this.techs.set(tech.guid, tech);
-        });
+    constructor(@InjectRepository(TechnologyEntity) technologyRepository: Repository<TechnologyEntity>) {
+        this.technologyRepository = technologyRepository;
+        this.syncDatabaseWithDefaults();
+    }
+
+    /**
+     * Retrieves all technologies.
+     * @returns An array of all technologies.
+     */
+    getAll(): ITechnology[] {
+        return Array.from(this.techs.values());
     }
 
     /**
@@ -41,8 +51,13 @@ export class TechnologyService {
      * @param guid The GUID of the technology.
      * @returns The technology if found, otherwise null.
      */
-    getByGuid(guid: string): ITechnology | null {
-        return this.techs.get(guid) || null;
+    async getByGuid(guid: string): Promise<ITechnology | null> {
+        const results = await this.technologyRepository.findOneBy({ guid });
+
+        if (results) {
+            return results as ITechnology;
+        }
+        return Promise.resolve(null);
     }
 
     update(tech: ITechnology): void {
@@ -53,11 +68,20 @@ export class TechnologyService {
         }
     }
 
-    /**
-     * Retrieves all technologies.
-     * @returns An array of all technologies.
-     */
-    getAll(): ITechnology[] {
-        return Array.from(this.techs.values());
+    private async syncDatabaseWithDefaults(): Promise<void> {
+        if (process.env.DB_CLEARDB_ON_STARTUP === "true") {
+            await this.technologyRepository.clear();
+        }
+
+        const defaultTechs: ITechnology[] = DEFAULT_TECHNOLOGIES;
+        const existingTechs = await this.technologyRepository.find();
+        const existingGuids = new Set(existingTechs.map((t) => t.guid));
+
+        for (const tech of defaultTechs) {
+            if (!existingGuids.has(tech.guid)) {
+                const techEntity = this.technologyRepository.create(tech as TechnologyEntity);
+                await this.technologyRepository.save(techEntity);
+            }
+        }
     }
 }
