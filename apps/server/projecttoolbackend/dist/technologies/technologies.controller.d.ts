395e6166252acc43de2cd47cb8520bda15c8613b
Henrik Collin <henrik.collin@cinia.fi>
2025-11-25 14:14:32 +0200

    Remove /dist from git tracking

--- apps/server/projecttoolbackend/dist/technologies/technologies.controller.d.ts
+++
@@ -1,5 +0,0 @@
-import { ITechnology } from "@frosttroll/projecttoolmodels";
-export declare class TechnologiesController {
-    getAllTechnologies(): ITechnology[];
-}
-//# sourceMappingURL=technologies.controller.d.ts.map
